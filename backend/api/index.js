33a083eb3a2e3d71bd64af91ebb28ff02cefbae1
Jatin <patrajatin0@gmail.com>
2026-03-02 18:10:22 +0530

    change the index.js file code

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,26 +4,25 @@ dotenv.config({
 })
 const app = require("../src/app")
 const connectDB = require("../src/model/database")
-const { model } = require("mongoose")
 
 
 const port = process.env.PORT
 
-// connectDB()
-//     .then((result) => {
-//         console.log("the database connection establisher...");
-//         // app.listen(port, () => {
-//         //     console.log("The port is listening on the localhost:" + port);
+connectDB()
+    .then((result) => {
+        console.log("the database connection establisher...");
+        // app.listen(port, () => {
+        //     console.log("The port is listening on the localhost:" + port);
 
-//         // })
+        // })
 
-//     }).catch((err) => {
-//         console.log("the db is not connected");
+    }).catch((err) => {
+        console.log("the db is not connected");
 
-//     })
+    })
 
 
-let isConnected = false
+
 async function connectToMongoDB() {
     try {
         connectDB()
@@ -40,13 +39,7 @@ async function connectToMongoDB() {
             })
 
     } catch (error) {
-        throw new Error("ERROR" + error.message)
+
     }
 
 }
-app.use((req, res, next) => {
-    if (!isConnected) {
-        connectToMongoDB()
-    }
-    next()
-})
